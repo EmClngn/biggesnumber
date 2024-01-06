180eafb88119cd9b7d36b9fc630b1bbe5811a750
unknown <emgcalangian@gmail.com>
2024-01-06 19:58:39 +0800

    3userinputs

--- biggestnumber.py
+++ biggestnumber.py
@@ -3,7 +3,14 @@
 
 # pseudo code
 
+
 # Ask user for 3 inputs of number
+first_number = input("What is your first number: ")
+second_number = input("What is your second number: ")
+third_number = input("What is your third number: ")
+# Just to check
+print(first_number, second_number, third_number)
+
 # Make a way to find the biggest number among the 3 user inputs using if-else statement
 # Print the biggest number
 # Do something to handle errors
