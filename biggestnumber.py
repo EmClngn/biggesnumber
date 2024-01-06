0a788f4134b2fa1e426771623ce50840ce85954a
unknown <emgcalangian@gmail.com>
2024-01-06 20:31:47 +0800

    Error Handling

--- biggestnumber.py
+++ biggestnumber.py
@@ -3,22 +3,25 @@
 
 # pseudo code
 
-
+# Do something to handle errors(Errors are decimals and characters besides numbers)
+while True:
+    try:
 # Ask user for 3 inputs of number
-first_number = input("What is your first number: ")
-second_number = input("What is your second number: ")
-third_number = input("What is your third number: ")
-
+        first_number = float(input("What is your first number: "))
+        second_number = float(input("What is your second number: "))
+        third_number = float(input("What is your third number: "))
 
 # Make a way to find the biggest number among the 3 user inputs using if-else statement
-if first_number >= second_number and first_number >= third_number:
-    biggest_number = first_number
-elif second_number >= third_number:
-    biggest_number = second_number
-else:
-    biggest_number = third_number
-
+        if first_number >= second_number and first_number >= third_number:
+            biggest_number = first_number
+        elif second_number >= third_number:
+            biggest_number = second_number
+        else:
+            biggest_number = third_number
 # Print the biggest number
-print(biggest_number)
-# Do something to handle errors
+        print("The biggest number on your input is", biggest_number)
+        break
+    except ValueError:
+        print("Only input numerical values. Please try again...")
+      
 # Transform the code for tkinter
