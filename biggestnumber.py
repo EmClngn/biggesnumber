9d841fd936d71e21156d735749d234ba7bcf0afa
unknown <emgcalangian@gmail.com>
2024-01-06 19:52:36 +0800

    realpsuedocode

---
+++ biggestnumber.py
@@ -0,0 +1,10 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+
+
+# pseudo code
+
+# Ask user for 3 inputs of number
+# Make a way to find the biggest number among the 3 user inputs using if-else statement
+# Print the biggest number
+# Do something to handle errors
+# Transform the code for tkinter
