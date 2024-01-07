340970ff1c524f0240544385c8400a666f2fc65a
unknown <emgcalangian@gmail.com>
2024-01-07 11:59:33 +0800

    finally working with error handling

--- biggestnumber.py
+++ biggestnumber.py
@@ -3,52 +3,44 @@
 # Transform the code for tkinter
 from tkinter import *
 
+    
 window = Tk()
-
-# pseudo code
-
 # Do something to handle errors(Errors are decimals and characters besides numbers)
+def finding_the_biggest ():
+    try: 
+        first_number = float(first_entry.get())
+        second_number = float(second_entry.get())
+        third_number = float(third_entry.get())
+
+        if first_number >= second_number and first_number >= third_number:
+            biggest_number = first_number
+        elif second_number >= third_number:
+            biggest_number = second_number
+        else:
+            biggest_number = third_number
+
+        the_biggest.config(text = f"The biggest number on your input is{biggest_number}")
+    except ValueError:
+        the_biggest.config(text = "Please only input Numerical Values")
+   
 
 # Ask user for 3 inputs of number
 number_list = (Label(window, text = "Give me your numbers"))
 number_list.pack()
 
-first_entry = (Entry(window))
-second_entry = (Entry(window))
-third_entry = (Entry(window))
+first_entry = Entry(window)
+second_entry = Entry(window)
+third_entry = Entry(window)
 
 first_entry.pack()
 second_entry.pack()
 third_entry.pack()
 
-def finding_the_biggest ():
-    first_number = (first_entry.get())
-    second_number = (second_entry.get())
-    third_number = (third_entry.get())
-
-    if first_number >= second_number and first_number >= third_number:
-        biggest_number = first_number
-    elif second_number >= third_number:
-        biggest_number = second_number
-    else:
-        biggest_number = third_number
-    the_biggest = Label(window,
-                text = f"The biggest number on your input is{biggest_number}")
-    the_biggest.pack()
-
-
-submit_button = Button(window, text = "Submit",
-                       command = finding_the_biggest())
+submit_button = Button(window, text = "Submit", command = finding_the_biggest)
 submit_button.pack()
-    
 
-
-# Make a way to find the biggest number among the 3 user inputs using if-else statement
-        
-# Print the biggest number
-        
-      
-# Transform the code for tkinter
+the_biggest = Label(window, text = "")
+the_biggest.pack()
 
 window.mainloop()
 
